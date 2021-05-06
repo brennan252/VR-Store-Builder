d8023f5651def72e6eb305369225bec0eebe87e5
brennan252 <79476154+brennan252@users.noreply.github.com>
2021-05-05 19:09:12 -0700

    Update rotateToParallel.js

--- buildUtilities/rotateToParallel.js
+++ buildUtilities/rotateToParallel.js
@@ -8,7 +8,7 @@ module.exports = function rotateToParallel(planes, viewPoint) {
         var location = plane.position.split(" ");
 
         // get x rotation
-        var xRot = ((Math.atan2(viewPoint[1] - location[1], -1 * Math.abs(location[2]) - viewPoint[2]) * 180 / Math.PI) + 180) % 360;;
+        var xRot = ((Math.atan2(viewPoint[1] - location[1], -1 * Math.abs(location[2]) - viewPoint[2]) * 180 / Math.PI) + 180) % 360;
 
         //get y rotation
         var yRot = ((Math.atan2(location[0] - viewPoint[0], location[2] - viewPoint[2]) * 180 / Math.PI) + 180) % 360;
